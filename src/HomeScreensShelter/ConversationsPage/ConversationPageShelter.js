346bb7ba9d5b757414bf69a2e35445865bf746c3
Philippe Tan <philippetan99@gmail.com>
2024-08-02 15:26:02 +0800

    Conversations page: added account picture, updated last message function

--- src/HomeScreensShelter/ConversationsPage/ConversationPageShelter.js
+++ src/HomeScreensShelter/ConversationsPage/ConversationPageShelter.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import { db, auth } from "../../FirebaseConfig";
 import {
   collection,
@@ -9,12 +9,15 @@ import {
   getDoc,
   doc,
   updateDoc,
+  limit,
 } from "firebase/firestore";
 import styles from "./styles";
 
 const ConversationPageShelter = ({ navigation }) => {
   const [conversations, setConversations] = useState([]);
   const [userNames, setUserNames] = useState({});
+  const [userImage, setUserImage] = useState({});
+  const [lastMessages, setLastMessages] = useState({});
   const [loading, setLoading] = useState(true);
   const shelterId = auth.currentUser.uid;
 
@@ -34,12 +37,18 @@ const ConversationPageShelter = ({ navigation }) => {
           );
 
           const names = {};
+          const accountPic = {};
+          const messages = {};
           for (const conversation of filteredConversations) {
             const userId = conversation.participants[0]; // Assuming the userId is the first participant
             names[conversation.id] = await getUserName(userId);
+            accountPic[conversation.id] = await getUserImage(userId);
+            messages[conversation.id] = await getLastMessage(conversation.id);
           }
 
           setUserNames(names);
+          setUserImage(accountPic);
+          setLastMessages(messages);
           setConversations(filteredConversations);
           setLoading(false);
         });
@@ -65,6 +74,56 @@ const ConversationPageShelter = ({ navigation }) => {
     } catch (error) {
       console.error("Error fetching user name:", error);
       return "Unknown User";
+    }
+  };
+
+  const getUserImage = async (userId) => {
+    try {
+      const userDoc = await getDoc(doc(db, "users", userId));
+      if (userDoc.exists()) {
+        return userDoc.data().accountPicture;
+      } else {
+        console.error("User document not found for userId: ", userId);
+        return "Unknown User";
+      }
+    } catch (error) {
+      console.error("Error fetching user name:", error);
+      return "Unknown User";
+    }
+  };
+
+  const getLastMessage = async (conversationId) => {
+    try {
+      const messagesRef = collection(
+        db,
+        "conversations",
+        conversationId,
+        "messages"
+      );
+      const q = query(messagesRef, orderBy("timestamp", "desc"), limit(1));
+      const snapshot = await new Promise((resolve, reject) => {
+        const unsubscribe = onSnapshot(
+          q,
+          (snapshot) => {
+            resolve(snapshot);
+            unsubscribe();
+          },
+          (error) => {
+            reject(error);
+            unsubscribe();
+          }
+        );
+      });
+
+      if (!snapshot.empty) {
+        return snapshot.docs[0].data();
+      } else {
+        console.error("No messages found for conversationId:", conversationId);
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching last message:", error);
+      return null;
     }
   };
 
@@ -104,24 +163,59 @@ const ConversationPageShelter = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Chats</Text>
       <FlatList
         data={conversations}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={[
-              styles.conversationItem,
-              // Check if receiverRead is false, and apply highlight if true
-              !item.receiverRead && styles.unreadConversation,
-            ]}
-            onPress={() =>
-              navigateToMessages(item.id, item.petId, item.participants[0])
-            }
-          >
-            <Text style={styles.userName}>From: {userNames[item.id]}</Text>
-            <Text style={styles.lastMessage}>{item.lastMessage}</Text>
-          </TouchableOpacity>
-        )}
+        renderItem={({ item }) => {
+          let lastMessageText = item.lastMessage;
+          if (item.lastMessage === "Image") {
+            lastMessageText =
+              item.participants[1] === lastMessages[item.id]?.senderId
+                ? "You sent a photo"
+                : `${userNames[item.id]} sent a photo`;
+          }
+          return (
+            <TouchableOpacity
+              style={[
+                styles.conversationItem,
+                !item.receiverRead && styles.unreadConversation,
+              ]}
+              onPress={() =>
+                navigateToMessages(item.id, item.petId, item.participants[0])
+              }
+            >
+              <View style={styles.userInfoContainer}>
+                <Image
+                  source={
+                    userImage[item.id]
+                      ? { uri: userImage[item.id] }
+                      : require("../../components/user.png")
+                  }
+                  style={styles.userImage}
+                />
+                <View style={styles.textContainer}>
+                  <Text
+                    style={[
+                      styles.userName,
+                      !item.receiverRead && styles.unreadConversation,
+                    ]}
+                  >
+                    {userNames[item.id]}
+                  </Text>
+                  <Text
+                    style={[
+                      styles.lastMessage,
+                      !item.receiverRead && styles.unreadConversation,
+                    ]}
+                  >
+                    {lastMessageText}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
       />
     </View>
   );
