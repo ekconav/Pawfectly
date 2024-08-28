7e801a262cfa0340cff8d9e5b5ae41c7eaed981b
Philippe Tan <philippetan99@gmail.com>
2024-08-28 14:43:35 +0800

    privacyPolicy page done, no content yet

--- src/HomeScreens/SettingsPage/SettingStack.js
+++ src/HomeScreens/SettingsPage/SettingStack.js
@@ -4,7 +4,7 @@ import SettingsScreen from "./SettingsPage";
 import AboutScreen from "./Setting Options/About Page/AboutPage";
 import ChangePassScreen from "./Setting Options/Change Password/ChangePassPage";
 import TOSScreen from "./Setting Options/TOS Page/TOS";
-import PrivacyScreen from "./Setting Options/PrivacyPage";
+import PrivacyScreen from "./Setting Options/PrivacyPolicyPage/PrivacyPage";
 import DeleteAccScreen from "./Setting Options/DeleteAccPage";
 import AccountScreen from "./Setting Options/Account Page/AccountPage";
 
@@ -18,7 +18,7 @@ export const SettingOptions = () => {
       <Stack.Screen name="About" component={AboutScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Change Password" component={ChangePassScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Terms of Service" component={TOSScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Privacy Policy" component={PrivacyScreen} />
+      <Stack.Screen name="Privacy Policy" component={PrivacyScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Request Account Deletion" component={DeleteAccScreen} />
     </Stack.Navigator>
   );
