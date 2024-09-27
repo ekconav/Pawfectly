1d5669bd9881a51ba152b245ecadfe81d88d3e48
DESKTOP-VM183H2\Justin Rey <collado.justinrey@gmail.com>
2024-09-27 16:32:28 +0800

    Added alert in adopters page

--- admin-dashboard/src/components/screens/usersPage/usersPage.js
+++ admin-dashboard/src/components/screens/usersPage/usersPage.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import Header from "../../header/header";
 import styles from "./styles";
 import { db, auth } from "../../../FirebaseConfig";
-import {writeBatch,collection,onSnapshot,updateDoc,deleteDoc,getDocs,doc,query,limit,} from "firebase/firestore";
+import {
+  writeBatch,
+  collection,
+  onSnapshot,
+  updateDoc,
+  deleteDoc,
+  getDocs,
+  doc,
+  query,
+  limit,
+} from "firebase/firestore";
 import Modal from "./usersModal";
+import Alerts from "./alert";
 
 import LoadingSpinner from "../loadingPage/loadingSpinner";
 
@@ -18,8 +29,10 @@ const UsersPage = () => {
 
   useEffect(() => {
     // Define the function to handle real-time updates
-    const q = query(collection(db, "users")); 
-    const unsubscribe = onSnapshot(q, (snapshot) => {
+    const q = query(collection(db, "users"));
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot) => {
         // Convert snapshot to array of users
         const usersList = snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -51,7 +64,6 @@ const UsersPage = () => {
       setSortedUsers(sortedList);
     }
   }, [users]);
-
 
   // Pagination
   // Calculate the total number of pages and current items
@@ -101,25 +113,24 @@ const UsersPage = () => {
     lastName: "",
     mobileNumber: "",
   });
-  
-  const handleEditUser = (user) => {
 
+  const handleEditUser = (user) => {
     // Must not include country code in the display
-    const mobileNumberWithoutCountryCode = user.mobileNumber.startsWith('+63')
-    ? user.mobileNumber.slice(3)
-    : user.mobileNumber;
-    
+    const mobileNumberWithoutCountryCode = user.mobileNumber.startsWith("+63")
+      ? user.mobileNumber.slice(3)
+      : user.mobileNumber;
+
     setUpdateUser({
       firstName: user.firstName || "",
       lastName: user.lastName || "",
       mobileNumber: mobileNumberWithoutCountryCode || "",
       verified: user.verified || false,
     });
-    setIsUpdateUserModalOpen(true); 
+    setIsUpdateUserModalOpen(true);
   };
 
-   // Verification Switch
-   const handleSwitchChange = (event) => {
+  // Verification Switch
+  const handleSwitchChange = (event) => {
     setUpdateUser((prev) => ({
       ...prev,
       verified: event.target.checked,
@@ -133,21 +144,27 @@ const UsersPage = () => {
     }));
   };
 
- 
-  
   // Function to handle user update
   const handleUpdateUser = async () => {
-
-    if (!updateUser.firstName || !updateUser.lastName || !updateUser.mobileNumber) {
-      alert("All fields are required.");
+    if (
+      !updateUser.firstName ||
+      !updateUser.lastName ||
+      !updateUser.mobileNumber
+    ) {
+      setAlertMessage('All fields are required.');
+      setAlertType('error');
       return;
     }
 
     // Validate mobile number length
-    if (updateUser.mobileNumber.length !== 10 || !updateUser.mobileNumber.startsWith('9')) {
-      alert("Improper mobile number according to the Country Code");
+    if (
+      updateUser.mobileNumber.length !== 10 ||
+      !updateUser.mobileNumber.startsWith("9")
+    ) {
+      setAlertMessage('Improper mobile number according to the Country Code');
+      setAlertType('error');
       return;
-  }
+    }
 
     try {
       const formattedMobileNumber = `+63${updateUser.mobileNumber}`;
@@ -157,7 +174,7 @@ const UsersPage = () => {
         firstName: updateUser.firstName,
         lastName: updateUser.lastName,
         mobileNumber: formattedMobileNumber,
-        verified: updateUser.verified, 
+        verified: updateUser.verified,
       });
 
       // Update the selectedUser state with the new data
@@ -166,7 +183,7 @@ const UsersPage = () => {
         firstName: updateUser.firstName,
         lastName: updateUser.lastName,
         mobileNumber: formattedMobileNumber,
-        verified: updateUser.verified, 
+        verified: updateUser.verified,
       }));
 
       // Update the users array to reflect the changes
@@ -178,10 +195,14 @@ const UsersPage = () => {
         );
         return updatedUsers;
       });
-      
-      setIsUpdateUserModalOpen(false); // Close the modal after update
+
+      setIsUpdateUserModalOpen(false); 
+      setAlertMessage('Adopter has been successdully updated.');
+      setAlertType('success');
     } catch (error) {
-      console.error("Error updating user:", error);
+      setAlertMessage('Error updating user.');
+      setAlertType('error');
+      
     }
   };
 
@@ -189,108 +210,152 @@ const UsersPage = () => {
   // Delete Modal
   const [isDeleteUserModalOpen, setDeleteUserModalOpen] = useState(false);
   const handleDeleteButton = (user) => {
-    setSelectedUser(user); 
-    setDeleteUserModalOpen(true); 
+    setSelectedUser(user);
+    setDeleteUserModalOpen(true);
   };
 
-// Function to delete a  sub-collection 
-const deleteSubCollection = async (userId, subCollectionName, nestedCollectionName) => {
-  try {
-    // Reference to the sub-collection (e.g., conversations)
-    const subCollectionRef = collection(db, "users", userId, subCollectionName);
-    const subCollectionSnapshot = await getDocs(subCollectionRef);
+  // Function to delete a  sub-collection
+  const deleteSubCollection = async (
+    userId,
+    subCollectionName,
+    nestedCollectionName
+  ) => {
+    try {
+      // Reference to the sub-collection (e.g., conversations)
+      const subCollectionRef = collection(
+        db,
+        "users",
+        userId,
+        subCollectionName
+      );
+      const subCollectionSnapshot = await getDocs(subCollectionRef);
 
-    if (subCollectionSnapshot.empty) {
-      console.log("No documents found");
-      return;
-    }
-
-    const batch = writeBatch(db); 
-
-    if(subCollectionName === "conversations"){
-      for (const subDoc of subCollectionSnapshot.docs) {
-        const data = subDoc.data();
-        const participants = data.participants || [];
-        const petId = data.petId || "";
-
-        // Construct the subdocId from fields in the document
-        const subdocId = `${participants[0]}_${participants[1]}_${petId}`;
-
-        
-        const nestedSubCollectionRef = collection(db, "users", userId, subCollectionName, subdocId, nestedCollectionName);
-        const nestedSubCollectionSnapshot = await getDocs(nestedSubCollectionRef);
-
-        // Delete each document in the nested sub-collection 
-        nestedSubCollectionSnapshot.forEach((nestedDoc) => {
-          const nestedDocRef = nestedDoc.ref;
-          batch.delete(nestedDocRef); 
-        });
-
-        // Delete the sub-collection document itself 
-        const subDocRef = doc(db, "users", userId, subCollectionName, subdocId);
-        batch.delete(subDocRef);
+      if (subCollectionSnapshot.empty) {
+        console.log("No documents found");
+        return;
       }
-    }else{
-      subCollectionSnapshot.forEach((doc) => {
-        const docRef = doc.ref;
-        batch.delete(docRef);
-      });
-    }
-    await batch.commit(); 
-  } catch (error) {
-    console.error(`Error deleting:`, error);
-  }
-};
 
+      const batch = writeBatch(db);
+
+      if (subCollectionName === "conversations") {
+        for (const subDoc of subCollectionSnapshot.docs) {
+          const data = subDoc.data();
+          const participants = data.participants || [];
+          const petId = data.petId || "";
+
+          // Construct the subdocId from fields in the document
+          const subdocId = `${participants[0]}_${participants[1]}_${petId}`;
+
+          const nestedSubCollectionRef = collection(
+            db,
+            "users",
+            userId,
+            subCollectionName,
+            subdocId,
+            nestedCollectionName
+          );
+          const nestedSubCollectionSnapshot = await getDocs(
+            nestedSubCollectionRef
+          );
+
+          // Delete each document in the nested sub-collection
+          nestedSubCollectionSnapshot.forEach((nestedDoc) => {
+            const nestedDocRef = nestedDoc.ref;
+            batch.delete(nestedDocRef);
+          });
+
+          // Delete the sub-collection document itself
+          const subDocRef = doc(
+            db,
+            "users",
+            userId,
+            subCollectionName,
+            subdocId
+          );
+          batch.delete(subDocRef);
+        }
+      } else {
+        subCollectionSnapshot.forEach((doc) => {
+          const docRef = doc.ref;
+          batch.delete(docRef);
+        });
+      }
+      await batch.commit();
+    } catch (error) {
+      console.error(`Error deleting:`, error);
+    }
+  };
 
   // Main delete user function
   const handleDeleteUser = async () => {
     if (!selectedUser) return;
-  
+
     try {
       // Attempt to delete the user from Firebase Authentication via backend API first
-      const response = await fetch(`http://localhost:5000/deleteUser/${selectedUser.id}`, {
-        method: 'DELETE',
-      });
-  
+      const response = await fetch(
+        `http://localhost:5000/deleteUser/${selectedUser.id}`,
+        {
+          method: "DELETE",
+        }
+      );
+
       // If the backend call fails, don't proceed with Firestore deletion
       if (!response.ok) {
+        setAlertMessage('Failed to delete user from Firebase Authentication.');
+        setAlertType('error');
         throw new Error("Failed to delete user from Firebase Authentication.");
       }
-  
+      
+
       // Now proceed with deleting the user's sub-collections from Firestore
-      await deleteSubCollection(selectedUser.id, 'conversations', 'messages');
-      await deleteSubCollection(selectedUser.id, 'favorites');
-      await deleteSubCollection(selectedUser.id, 'furbabies');
-      await deleteSubCollection(selectedUser.id, 'petsAdopted');
-  
+      await deleteSubCollection(selectedUser.id, "conversations", "messages");
+      await deleteSubCollection(selectedUser.id, "favorites");
+      await deleteSubCollection(selectedUser.id, "furbabies");
+      await deleteSubCollection(selectedUser.id, "petsAdopted");
+
       // Then delete the main user document
       const userDocRef = doc(db, "users", selectedUser.id);
       await deleteDoc(userDocRef);
-  
+
       // Update local state to remove the user
-      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== selectedUser.id));
-  
+      setUsers((prevUsers) =>
+        prevUsers.filter((user) => user.id !== selectedUser.id)
+      );
+
       // Close modal and reset selected user
       setDeleteUserModalOpen(false);
       setSelectedUser(null);
-  
+      setAlertMessage('Adopter has been successfully deleted.');
+      setAlertType('success');
     } catch (error) {
-      console.error("Error deleting user and sub-collections:", error);
-      alert("Failed to delete user and associated data.");
+      setAlertMessage('Failed to delete user and associated data.');
+      setAlertType('error');
     }
   };
 
   // Inside your component
-const [isImageModalOpen, setIsImageModalOpen] = useState(false);
-const [selectedImageUrl, setSelectedImageUrl] = useState("");
+  const [isImageModalOpen, setIsImageModalOpen] = useState(false);
+  const [selectedImageUrl, setSelectedImageUrl] = useState("");
 
-// Function to open the image modal
-const openImageModal = (url) => {
-  setSelectedImageUrl(url);
-  setIsImageModalOpen(true);
-};
-  
+  // Function to open the image modal
+  const openImageModal = (url) => {
+    setSelectedImageUrl(url);
+    setIsImageModalOpen(true);
+  };
+
+  //Alert Message and Type
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
+
+  useEffect(() => {
+    if (alertMessage) {
+      const timer = setTimeout(() => {
+        setAlertMessage(""); // Clear the message
+      }, 3000); // Hide after 3 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [alertMessage]);
 
   if (loading) {
     return <LoadingSpinner />;
@@ -303,16 +368,16 @@ const openImageModal = (url) => {
       <div style={styles.container}>
         <div style={styles.userListContainer}>
           <div style={styles.userDetailsLabel}>
-              <div style={styles.line}>
-                <p style={styles.title}>Image</p>
-              </div>
-              <div style={styles.line}>
-                <p style={styles.title}>Name</p>
-              </div>
-              <div style={styles.line}>
-                <p style={styles.title}>Status</p>
-              </div>
+            <div style={styles.line}>
+              <p style={styles.title}>Image</p>
             </div>
+            <div style={styles.line}>
+              <p style={styles.title}>Name</p>
+            </div>
+            <div style={styles.line}>
+              <p style={styles.title}>Status</p>
+            </div>
+          </div>
           <div style={styles.userDetails}>
             {currentItems.map((users) => (
               <React.Fragment key={users.id}>
@@ -344,12 +409,16 @@ const openImageModal = (url) => {
                     {getVerificationStatus(users.verified)}
                   </p>
                 </div>
-                <div style={styles.line} >
-                <ion-icon
-                  style={{ fontSize: '30px', color: 'red', cursor: 'pointer' }}
-                  name="trash-outline"
-                  onClick={() => handleDeleteButton(users)}
-                ></ion-icon>
+                <div style={styles.line}>
+                  <ion-icon
+                    style={{
+                      fontSize: "30px",
+                      color: "red",
+                      cursor: "pointer",
+                    }}
+                    name="trash-outline"
+                    onClick={() => handleDeleteButton(users)}
+                  ></ion-icon>
                 </div>
               </React.Fragment>
             ))}
@@ -379,12 +448,10 @@ const openImageModal = (url) => {
             />
             <div style={styles.userInfoDetails}>
               <div style={styles.line}>
-                <p style={styles.userInfoTitleLabel}>
-                  Name:
-                </p>
+                <p style={styles.userInfoTitleLabel}>Name:</p>
               </div>
               <div style={styles.line}>
-                <p style={styles.userInfoTitleLabel} >
+                <p style={styles.userInfoTitleLabel}>
                   {selectedUser.firstName} {selectedUser.lastName}
                 </p>
               </div>
@@ -398,13 +465,20 @@ const openImageModal = (url) => {
                 <p style={styles.userInfoTitleLabel}>Mobile Number:</p>
               </div>
               <div style={styles.line}>
-                <p style={styles.userInfoTitleLabel}>{selectedUser.mobileNumber}</p>
+                <p style={styles.userInfoTitleLabel}>
+                  {selectedUser.mobileNumber}
+                </p>
               </div>
               <div style={styles.line}>
                 <p style={styles.userInfoTitleLabel}>Status:</p>
               </div>
               <div style={styles.line}>
-                <p style={{ ...styles.userInfoTitleLabel, color: getStatusColor(selectedUser.verified) }}>
+                <p
+                  style={{
+                    ...styles.userInfoTitleLabel,
+                    color: getStatusColor(selectedUser.verified),
+                  }}
+                >
                   {getVerificationStatus(selectedUser.verified)}
                 </p>
               </div>
@@ -413,7 +487,10 @@ const openImageModal = (url) => {
               </div>
               <div style={styles.line}>
                 <img
-                  src={selectedUser.governmentId || require("../../../const/user.png")}
+                  src={
+                    selectedUser.governmentId ||
+                    require("../../../const/user.png")
+                  }
                   alt="ID"
                   style={styles.govtPicture}
                   onClick={() => {
@@ -422,7 +499,6 @@ const openImageModal = (url) => {
                   }}
                 />
               </div>
-              
             </div>
           </div>
         )}
@@ -431,7 +507,9 @@ const openImageModal = (url) => {
       {/* Pagination */}
       {totalPages > 1 && (
         <div style={styles.pagination}>
-          <span>{currentPage} / {totalPages}</span>
+          <span>
+            {currentPage} / {totalPages}
+          </span>
           <button
             style={{
               ...styles.paginationButton,
@@ -459,26 +537,41 @@ const openImageModal = (url) => {
       {isUpdateUserModalOpen && (
         <Modal.UpdateModal
           updateUser={updateUser}
-          handleInputChange={(e) => setUpdateUser({ ...updateUser, [e.target.name]: e.target.value })}
-          handleSwitchChange={(e) => setUpdateUser({ ...updateUser, verified: e.target.checked })}
+          handleInputChange={(e) =>
+            setUpdateUser({ ...updateUser, [e.target.name]: e.target.value })
+          }
+          handleSwitchChange={(e) =>
+            setUpdateUser({ ...updateUser, verified: e.target.checked })
+          }
           handleUpdateUser={handleUpdateUser}
           handleCloseUpdateUserModal={() => setIsUpdateUserModalOpen(false)}
         />
       )}
       {/* Delete Button Modal */}
       {isDeleteUserModalOpen && (
-        <Modal.DeleteModal onConfirm={handleDeleteUser} onClose={() => setDeleteUserModalOpen(false)}>
+        <Modal.DeleteModal
+          onConfirm={handleDeleteUser}
+          onClose={() => setDeleteUserModalOpen(false)}
+        >
           <p>Are you sure you want to delete {selectedUser.firstName}?</p>
         </Modal.DeleteModal>
       )}
 
       {/* Image Zoom Modal */}
       {isImageModalOpen && (
-        <Modal.ImageModal 
-          isOpen={isImageModalOpen} 
-          imageUrl={selectedImageUrl} 
-          onClose={() => setIsImageModalOpen(false)} 
+        <Modal.ImageModal
+          isOpen={isImageModalOpen}
+          imageUrl={selectedImageUrl}
+          onClose={() => setIsImageModalOpen(false)}
         />
+      )}
+
+      {/* Alert rendering based on the type */}
+      {alertMessage && alertType === "success" && (
+        <Alerts.SuccessAlert>{alertMessage}</Alerts.SuccessAlert>
+      )}
+      {alertMessage && alertType === "error" && (
+        <Alerts.ErrorAlert>{alertMessage}</Alerts.ErrorAlert>
       )}
     </div>
   );
