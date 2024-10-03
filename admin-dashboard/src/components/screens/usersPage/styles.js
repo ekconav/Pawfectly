6842a46c01139893d3cab7d1c80dd58c8c6114ce
DESKTOP-VM183H2\Justin Rey <collado.justinrey@gmail.com>
2024-10-03 15:56:22 +0800

    Enhanced Frontend View

--- admin-dashboard/src/components/screens/usersPage/styles.js
+++ admin-dashboard/src/components/screens/usersPage/styles.js
@@ -2,38 +2,45 @@ import { StyleSheet } from "react-native";
 import COLORS from "../../colors";
 
 const styles = StyleSheet.create({
-
-  container: {
-    display: 'grid',
-    gridTemplateColumns: '2fr 1fr', // Two equal columns
-    gap: '10px', // Space between columns
-    padding: '10px',
+  pageTitle: {
+    fontSize: "40px",
+    fontWeight: 500,
+    fontFamily: "Poppins",
+    color: COLORS.title,
+    margin: 10,
   },
 
-  //User List 
-  userListContainer: {                      
+  container: {
+    display: "grid",
+    gridTemplateColumns: "2fr 1fr", // Two equal columns
+    gap: "10px", // Space between columns
+    padding: "10px",
+  },
+
+  //User List
+  userListContainer: {
     display: "flex",
     paddingLeft: 50,
     paddingRight: 50,
     marginLeft: 50,
-    marginRight: 10,  
+    marginRight: 10,
     borderRadius: "10px",
     boxShadow: "0 8px 16px rgba(0, 0, 0, 0.4)",
-    height: "60vh",  
-    width:"60vw",
+    height: "60vh",
+    width: "60vw",
     flexDirection: "column",
   },
   userDetails: {
     display: "grid",
-    gridTemplateColumns: "15vh 50vh 20vh 10vh", 
-    gridTemplateRows: 'repeat(5, 2fr)', // Increase row height by setting it to 2fr
-    gap: "10px", 
+    gridTemplateColumns: "15vh 50vh 20vh 10vh",
+    gridTemplateRows: "repeat(5, 2fr)", // Increase row height by setting it to 2fr
+    gap: "10px",
   },
   userDetailsLabel: {
     display: "grid",
-    gridTemplateColumns: "15vh 50vh 20vh 10vh", 
-    height: "10vh", 
-    gap: "10px", 
+    gridTemplateColumns: "15vh 50vh 20vh 10vh",
+    height: "10vh",
+    gap: "10px",
   },
   line: {
     display: "flex",
@@ -41,36 +48,38 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: 500,
+    fontFamily: "Poppins",
+    color: COLORS.title,
   },
   userPicture: {
     width: 50,
-    height:  50,
+    height: 50,
     borderRadius: 25,
-  }, 
+  },
 
   // Pagination
   pagination: {
-    display: 'flex',
-    justifyContent: 'right',
-    alignItems: 'center',
-    marginTop: '20px',
+    display: "flex",
+    justifyContent: "right",
+    alignItems: "center",
+    marginTop: "20px",
     marginRight: 50,
   },
   paginationButton: {
-    fontFamily: 'inherit',
-    fontWeight: '600',
-    fontSize: '16px',
-    marginLeft: '10px',
-    borderRadius: '6px',
-    cursor: 'pointer',
-    transition: 'all 0.3s ease',
-    background: 'transparent',
-    color: '#5F5B5B',
-    border: '2px solid #5F5B5B',
+    fontFamily: "inherit",
+    fontWeight: "600",
+    fontSize: "16px",
+    marginLeft: "10px",
+    borderRadius: "6px",
+    cursor: "pointer",
+    transition: "all 0.3s ease",
+    background: "transparent",
+    color: "#5F5B5B",
+    border: "2px solid #5F5B5B",
   },
   disabledButton: {
     opacity: 0.5,
-    cursor: 'not-allowed',
+    cursor: "not-allowed",
   },
 
   // User Information
@@ -81,32 +90,31 @@ const styles = StyleSheet.create({
     marginRight: 50,
     borderRadius: "10px",
     boxShadow: "0 8px 16px rgba(0, 0, 0, 0.4)",
-    height: "60vh",  
-    width:"28vw",
+    height: "60vh",
+    width: "28vw",
   },
   userInfoHeader: {
-    display: 'flex',
-    justifyContent: 'space-between',  
-    alignItems: 'center',
-    width: '100%',                     
-    paddingBottom: '10px',    
-    paddingTop: '10px',          
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingBottom: "10px",
+    paddingTop: "10px",
   },
   userInfoTitle: {
-    flex: 1,                           
-    textAlign: 'center',             
-    fontSize: '18px',                 
+    flex: 1,
+    textAlign: "center",
+    fontSize: "18px",
   },
   selectedUserPicture: {
     width: 100,
-    height:  100,
+    height: 100,
     borderRadius: 25,
   },
   userInfoDetails: {
-    display: "grid",  
-    gridTemplateColumns: "20vh 30vh", 
-    // gridTemplateRows: 'repeat(5, 1fr)', 
-    gridTemplateRows: "1fr 1fr 1fr 1fr 2fr", 
+    display: "grid",
+    gridTemplateColumns: "11vh 30vh",
+    gridTemplateRows: "1fr 1fr 10vh 1fr 2fr",
     gap: "3px",
   },
   userInfoTitleLabel: {
@@ -115,48 +123,37 @@ const styles = StyleSheet.create({
   },
   govtPicture: {
     width: 80,
-    height:  60,
+    height: 60,
     borderRadius: 10,
-    cursor: 'pointer',
-  }, 
+    cursor: "pointer",
+  },
 
   // goPicture Modal
   fullImage: {
-    maxWidth: '100%',
-    maxHeight: '80vh',
+    maxWidth: "100%",
+    maxHeight: "80vh",
     border: "2px solid #5F5B5B",
     borderRadius: "10px",
     boxShadow: "0 8px 16px rgba(0, 0, 0, 0.4)",
-
   },
   closeButton: {
-    marginTop: '10px',
-    padding: '5px 10px',
-    backgroundColor: '#5F5B5B',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
+    marginTop: "10px",
+    padding: "5px 10px",
+    backgroundColor: "#5F5B5B",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
   },
-  
-
 
   // User Information Edit Button
-  editButtonContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-    border: "2px solid blue", 
-    borderRadius: "6px",
-    width: "20px", 
-    height: "20px", 
+  editIcon: {
     cursor: "pointer",
     transition: "all 0.3s ease",
-  },
-  editIcon: {
-    color: "blue",
-    fontSize: "30px",
+    border: `2px solid ${COLORS.prim}`,
+    borderRadius: "6px",
+    color: COLORS.prim,
+    fontSize: "20px",
   },
 
   //Modal Styles
@@ -191,16 +188,16 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     color: COLORS.white,
-    cursor: 'pointer',
+    cursor: "pointer",
   },
   cancelButton: {
-    backgroundColor: COLORS.outline,
-    color: COLORS.black,
+    backgroundColor: COLORS.subtitle,
+    color: COLORS.white,
     padding: 20,
     border: "none",
     borderRadius: 10,
     marginLeft: 10,
-    cursor: 'pointer',
+    cursor: "pointer",
   },
 
   // Modal Edit Forms
@@ -208,7 +205,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     alignItems: "start",
-    gap: 10
+    gap: 10,
   },
   inputField: {
     width: "100%",
@@ -219,24 +216,29 @@ const styles = StyleSheet.create({
     boxSizing: "border-box",
   },
   SwitchLine: {
-    display: 'flex',
-    alignItems: 'center',
-    gap: '10px',  
-    justifyContent: 'flex-start',  
-    width: '100%',  
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    justifyContent: "flex-start",
+    width: "100%",
   },
   mobileNumberContainer: {
-    position: 'relative',  
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
+    position: "relative",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
   },
   countryCode: {
-    position: 'absolute',  
-    left: '10px',
-    fontSize: '14px',
-    fontWeight: 'bold',
-    color: '#5F5B5B', 
+    position: "absolute",
+    left: "10px",
+    fontSize: "14px",
+    fontWeight: "bold",
+    color: "#5F5B5B",
+  },
+  modalTitle: {
+    fontWeight: 500,
+    fontFamily: "Poppins",
+    color: COLORS.title,
   },
 });
 
