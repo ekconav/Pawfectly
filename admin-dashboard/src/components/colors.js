d923cffd040ab660994c047a8a58b390ff061ba8
DESKTOP-VM183H2\Justin Rey <collado.justinrey@gmail.com>
2024-10-03 14:40:25 +0800

    Fixed Bug on TOS which automatically adds id to the document and refurnish the frontend view and Fixed loading spinner

--- admin-dashboard/src/components/colors.js
+++ admin-dashboard/src/components/colors.js
@@ -8,7 +8,23 @@ const COLORS = {
   hover: "#D573FF",
   addButtonHover: "#B4A5C9",
   deleteButtonHover: "#FF6E6E",
-  outline: "#CCCCCC",
+
+  
+
+  // proper final colors
+  subtitle: "#ADA8A8",
+  black: "#000000",
+  input: "#82739757",
+  link: "#0085FF",
+  male: "#8EB1E5",
+  malegenderbg: "#8EB1E533",
+  female: "#F672E1",
+  femalegenderbg: "#F672E133",
+  delete: "#FF001E",
+  sentMessage: "#827397CC",
+  receivedMessage: "#DFDEDECC",
+  sentDateTime: "#D4D4D4",
+
 };
 
 export default COLORS;
--- admin-dashboard/src/components/screens/loadingPage/loadingSpinner.js
+++ admin-dashboard/src/components/screens/loadingPage/loadingSpinner.js
@@ -9,7 +9,13 @@ const LoadingSpinner = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh",
+        position: "fixed", // Make it cover the entire screen
+        top: 0,
+        left: 0,
+        width: "100vw",    // Full width of the viewport
+        height: "100vh",   // Full height of the viewport
+        backgroundColor: COLORS.white, // Optional: white background with opacity
+        zIndex: 9999,      // Ensure it stays on top of everything
       }}
     >
       <ClipLoader color={COLORS.prim} loading={true} size={50} />
